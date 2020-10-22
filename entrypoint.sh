9be31295018671fed56b4325c645a617f0dc834b
van1yu3 <futurerxii@hotmail.com>
2020-10-22 15:36:22 +0800

    customize

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,7 +19,9 @@ npm install hexo-deployer-git --save
 # deployment
 if [ "$INPUT_COMMIT_MSG" = "none" ]
 then
-    hexo g -d
+    hexo g
+    cp -rf source/private/* public/
+    hexo d
 elif [ "$INPUT_COMMIT_MSG" = "" ] || [ "$INPUT_COMMIT_MSG" = "default" ]
 then
     # pull original publish repo
